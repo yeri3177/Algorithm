08de5e6f16ff2d4885debfbcb5058db90efcd458
YERI <jyr3177@gmail.com>
2024-03-11 21:01:40 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.py
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.py
@@ -1,10 +1,12 @@
+# my code
+# def solution(s):
+#     answer = ''
+#     middle = len(s)//2
+#     if len(s)%2 != 0:
+#         answer = s[middle:middle+1]
+#     else:
+#         answer = s[middle-1:middle+1]
+#     return answer
+
 def solution(s):
-    answer = ''
-    middle = len(s)//2
-    # print(middle)
-    
-    if len(s)%2 != 0:
-        answer = s[middle:middle+1]
-    else:
-        answer = s[middle-1:middle+1]
-    return answer
+    return s[(len(s)-1)//2 : len(s)//2 + 1]
