79f5a01d4361629ee5ea5d1eebe6ceed03d39c5a
YERI <jyr3177@gmail.com>
2024-03-11 19:15:22 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.py
@@ -0,0 +1,10 @@
+def solution(numbers):
+    answer = 0
+    
+    for i in range (1,10):
+        if i in numbers:
+            continue
+        else:
+            answer += i
+    
+    return answer
