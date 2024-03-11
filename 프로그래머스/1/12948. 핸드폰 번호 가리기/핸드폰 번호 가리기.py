14866e9da17a26b97bba72883006fc26ff5c7baa
YERI <jyr3177@gmail.com>
2024-03-11 19:04:04 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.py
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.py
@@ -1,9 +1,9 @@
+# def solution(phone_number):
+#     answer = ''   
+#     for i in range(0, len(phone_number)-4):
+#         answer += '*' 
+#     answer += phone_number[len(phone_number)-4:len(phone_number)]
+#     return answer
+
 def solution(phone_number):
-    answer = ''
-    
-    for i in range(0, len(phone_number)-4):
-        answer += '*'
-    
-    answer += phone_number[len(phone_number)-4:len(phone_number)]
-    
-    return answer
+    return "*"*(len(phone_number)-4)+phone_number[-4:]
