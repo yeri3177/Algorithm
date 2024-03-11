a5912950e847141f987ee3fddb9836c6e28b67b0
YERI <jyr3177@gmail.com>
2024-03-11 17:47:52 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.py
@@ -0,0 +1,6 @@
+def solution(seoul):
+#     answer = ''
+    
+#     print(seoul.index('Kim'))
+    # answer = '김서방은 '+ seoul.index('Kim') + '에 있다'
+    return '김서방은 '+ str(seoul.index('Kim')) + '에 있다'
