427c24b570614d4eaf5d4b6e67bb475eb60f9668
YERI <jyr3177@gmail.com>
2024-03-12 09:25:05 +0900

    Time: 1 ms (68.12%), Space: 42.1 MB (28.96%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -13,16 +13,44 @@
  *     }
  * }
  */
+
+// BFS 방식 
+class Solution { 
+    public List<Integer> rightSideView(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        List<Integer> ans = new ArrayList<>();
+
+        if(root == null) return ans;
+        queue.offer(root);
+
+        while(!queue.isEmpty()){
+            int size = queue.size();
+
+            for(int i = 0; i<size; i++){
+                TreeNode node = queue.poll();
+
+                if(i == size-1) ans.add(node.val); // 마지막 노드만 ans에 담기 
+                if(node.left != null) queue.offer(node.left);
+                if(node.right != null) queue.offer(node.right);
+            }
+        }
+        return ans;
+    }
+}
+
+/*
+// DFS 방식 
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
         List<Integer> list = new ArrayList<>();
-        bfs(root, list, 0);
+        dfs(root, list, 0);
         return list;
     }
-    public void bfs(TreeNode root, List<Integer> list, int depth){
-        if(root == null) return;
-        if(depth == list.size()) list.add(root.val);
-        bfs(root.right, list, depth+1);
-        bfs(root.left, list, depth+1);
+    public void dfs(TreeNode root, List<Integer> list, int depth){
+        if(root==null) return;
+        if(list.size() == depth) list.add(root.val);
+        dfs(root.right, list, depth+1);
+        dfs(root.left, list, depth+1);
     }
 }
+*/
