5863fe7ad69d4d6c1b9c2cfa5d4ee3c68b52ad94
YERI <jyr3177@gmail.com>
2024-03-13 19:23:02 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
@@ -0,0 +1,16 @@
+def solution(s):
+    answer = ''
+    idx = 0
+    
+    for i in s:
+        if i == ' ':
+            answer += ' '
+            idx=0
+        else:
+            if idx%2 == 0:
+                answer += i.upper()
+            else:
+                answer += i.lower()
+            idx+=1
+    
+    return answer
