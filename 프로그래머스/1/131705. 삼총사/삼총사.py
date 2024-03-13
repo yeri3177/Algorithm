0e4dffcea537efd5194424ac5967c5919874f27c
YERI <jyr3177@gmail.com>
2024-03-13 19:56:57 +0900

    [level 1] Title: 삼총사, Time: 0.06 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/131705. 삼총사/삼총사.py
@@ -0,0 +1,33 @@
+def solution(number):
+    answer = 0
+    # sorted(number)
+    for idx1, val1 in enumerate(number):
+        for idx2, val2 in enumerate(number):
+            for idx3, val3 in enumerate(number):
+                if idx1 >= idx2 or idx2 >= idx3:
+                    continue
+                if val1+val2+val3 == 0:
+                    answer += 1
+    
+    return answer
+
+#ref1
+def solution (number) :
+    from itertools import combinations
+    cnt = 0
+    for i in combinations(number,3) :
+        if sum(i) == 0 :
+            cnt += 1
+    return cnt
+
+#ref2
+def solution(number):
+    answer = 0
+    l = len(number)
+    for i in range(l-2):
+        for j in range(i+1, l-1):
+            for k in range(j+1, l):
+                # print(number[i],number[j],number[k])
+                if number[i]+number[j]+number[k] == 0:
+                    answer += 1           
+    return answer
