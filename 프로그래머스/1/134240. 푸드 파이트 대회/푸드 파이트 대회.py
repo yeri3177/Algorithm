626974ced438ec9d5f65f1bdeedd729426cf9d3d
YERI <jyr3177@gmail.com>
2024-03-14 20:33:27 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
@@ -2,8 +2,8 @@ def solution(food):
     answer = ''
     
     for i in range(1, len(food)):
-        halfCnt = food[i]//2
-        answer += str(i)*halfCnt
+        num = food[i]//2
+        answer += str(i)*num
      
     answer = answer + '0' + answer[::-1]
     
