472bde51faa8a0c1149bafeb2aa58877f40eb0ba
YERI <jyr3177@gmail.com>
2024-03-15 14:37:36 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
@@ -1,4 +1,4 @@
-# 약수 구하기
+# 약수 구하는 함수 1 : O(n)
 def divisor(num):
     cnt = 0
     
@@ -8,6 +8,19 @@ def divisor(num):
 
     return cnt
 
+# 약수 구하는 함수 2 : O(n**0.5)
+def divisor2(num):
+    cnt = 0
+
+    for i in range(1, int(num**(1/2)) + 1):
+        if (num % i == 0):
+            cnt += 1
+            if ( (i**2) != num) : 
+                cnt += 1
+    
+    return cnt
+
+# my code 
 def solution(left, right):
     answer = 0
     
@@ -19,3 +32,26 @@ def solution(left, right):
     
     return answer
 
+# ref
+## ex. 루트 2 = 2**(1/2) = 2**0.5 = math.sqrt(2)
+def solution(left, right):
+    answer = 0
+    for i in range(left, right+1):
+        if int(i**0.5) == i**0.5:
+            answer -= i
+        else:
+            answer += i
+    return answer
+
+# ref
+import math
+def solution(left, right):
+    answer = 0
+    for i in range(left, right+1):
+        sqrt = math.sqrt(i)
+        if int(sqrt) == sqrt:
+            answer -= i
+        else:
+            answer += i
+
+    return answer
