d2ea5d667e6a7ec5b4d2cb8de41ba97d236b1be6
YERI <jyr3177@gmail.com>
2024-03-16 23:02:49 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.04 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
@@ -1,3 +1,8 @@
 def solution(s):
     num = list(map(int, s.split(' ')))
     return str(min(num))+' '+str(max(num))
+
+#ref
+def solution(s):
+    arr = [int(s) for s in s.split()]
+    return f'{min(arr)} {max(arr)}'
