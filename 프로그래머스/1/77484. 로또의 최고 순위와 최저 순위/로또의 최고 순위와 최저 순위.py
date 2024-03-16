0e452a90f06c9055b57491f393b4422b7737882a
YERI <jyr3177@gmail.com>
2024-03-17 01:58:38 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.py
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.py
@@ -8,8 +8,35 @@ def solution(lottos, win_nums):
             count += 1
         if num == 0:
             zero += 1
-    print(str(count) + ' '+ str(zero))
+
     rank = [6, 6, 5, 4, 3, 2, 1]
     answer.append(rank[count+zero])
     answer.append(rank[count])
+    
     return answer
+
+# ref
+def solution(lottos, win_nums):
+
+    rank=[6,6,5,4,3,2,1]
+
+    cnt_0 = lottos.count(0)
+    ans = 0
+    for x in win_nums:
+        if x in lottos:
+            ans += 1
+    return rank[cnt_0 + ans],rank[ans]
+	
+# ref
+def solution(lottos, win_nums):
+    rank = {
+        0: 6,
+        1: 6,
+        2: 5,
+        3: 4,
+        4: 3,
+        5: 2,
+        6: 1
+    }
+    return [rank[len(set(lottos) & set(win_nums)) + lottos.count(0)], rank[len(set(lottos) & set(win_nums))]]
+	
