e108a61f3d1d371634257bbc002b400bb625e0eb
YERI <jyr3177@gmail.com>
2024-03-16 19:24:58 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 3.64 ms, Memory: 11.2 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
@@ -1,32 +1,17 @@
-"""
-now + relativedelta(months=1)
-my_date = datetime(2019,10,10)
-new_date1 = my_date + relativedelta(months=4)
-
-import datetime
-import relativedelta
-
-relativedelta.relativedelta(months=1)
-
- # val[-1:] : 약관종류 
-# val[:10] : 날짜 
-# print(datetime(2019,10,10)+relativedelta(months=1))
-"""
-
 from datetime import datetime
-from dateutil import relativedelta
+from dateutil.relativedelta import relativedelta
 
 def solution(today, terms, privacies):
     answer = []
     today = datetime.strptime(today, "%Y.%m.%d")
     
     for idx, val in enumerate(privacies):
-        day = datetime.strptime(val[:10], "%Y.%m.%d")
         month = 0
         for term in terms:
             if term.startswith(val[-1:]):
                 month = int(term[term.index(' '):])
-                
-        if today >= day + relativedelta.relativedelta(months=month):
+        day = datetime.strptime(val[:10], "%Y.%m.%d")
+        
+        if today >= day + relativedelta(months=month):
             answer.append(idx+1)
     return answer
