cb0c534e458c0b1819525e4ddabcde1f07292853
YERI <jyr3177@gmail.com>
2024-03-17 15:55:10 +0900

    [level 1] Title: 공원 산책, Time: 0.08 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/172928. 공원 산책/공원 산책.py
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.py
@@ -1,41 +1,52 @@
+# def solution(park, routes):
+#     answer = []
+#     x = 0
+#     y = 0
+    
+#     # 현재 위치 찾기 
+#     for idx, val in enumerate(park):
+#         if val.find('S') != -1:
+#             x = idx
+#             y = val.index('S')
+#             answer.append(x)
+#             answer.append(y)
+    
+#     # 이동하기 
+#     # for route in routes:
+#         # 방향 route[0:1]
+#         # 거리 route[2:]
+        
+        
+#     print(answer)
+
+#     return answer
+
 def solution(park, routes):
-    # 위치 index
-    x = 0
-    y = 0 
-    
+    answer = []
+    n=len(routes)
+
+    dic={'W':0,'S':1,'E':2,'N':3}
+    dx=[-1,0,1,0]
+    dy=[0,1,0,-1]
+
     for i in range(len(park)):
-        for j in range(len(park[i])):
-            if park[i][j] == 'S':
-                x = j
-                y = i
+        for j in range(len(park[0])):
+            if park[i][j]=='S':
+                a,b=i,j #a=y , b=x
+
+    for i in routes:
+        di = i[0]
+        le = int(i[2])
+        ty,tx = a,b
+        for j in range(le):
+            t1=a+dy[dic[di]]
+            t2=b+dx[dic[di]]
+            if t1>=0 and t2>=0 and t1<len(park) and t2<len(park[0]) and park[t1][t2]!='X':
+                a,b=t1,t2
+            else:
+                a,b = ty,tx
                 break
-    
-    # 이동
-    for route in routes:
-        # 위치 초기화
-        xx = x
-        yy = y
-        # 이동 - 장애물이 있거나 공원을 벗어나면 명령 무시
-        for step in range(int(route[2])):
-            # 동쪽 : 현재 위치가 map 가장 오른쪽이면 안됨, 이동할 곳이 장애물이면 안됨
-            if route[0] == 'E' and xx != len(park[0])-1 and park[yy][xx+1] != 'X':
-                xx += 1
-                if step == int(route[2])-1:
-                    x = xx # step만큼 움직였으면 위치 초기화
-            # 서쪽 : 현재 위치가 map 가장 왼쪽이면 안됨, 이동할 곳이 장애물이면 안됨
-            elif route[0] == 'W' and xx != 0 and park[yy][xx-1] != 'X':
-                xx -= 1
-                if step == int(route[2])-1:
-                    x = xx
-            # 남쪽 : 현재 위치가 map 가장 아래쪽이면 안됨, 이동할 곳이 장애물이면 안됨
-            elif route[0] == 'S' and yy != len(park)-1 and park[yy+1][xx] != 'X':
-                yy += 1
-                if step == int(route[2])-1:
-                    y = yy
-            # 북쪽 : 현재 위치가 map 가장 위쪽이면 안됨, 이동할 곳이 장애물이면 안됨
-            elif route[0] == 'N' and yy != 0 and park[yy-1][xx] != 'X':
-                yy -= 1
-                if step == int(route[2])-1:
-                    y = yy
-                    
-    return [y, x]
+    answer=[a,b]
+
+
+    return answer
