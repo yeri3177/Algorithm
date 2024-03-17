94b721bf3ae57206c6425f104f9aeb5c69da0056
YERI <jyr3177@gmail.com>
2024-03-18 00:45:38 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -1,9 +1,8 @@
-import math
 def solution(n, m):
     answer = []
     # 최대공약수
     for i in range(min(n,m), 0, -1): 
-        if n%i ==0 and m%i==0:
+        if n%i == 0 and m%i == 0:
             answer.append(i)
             break
 
@@ -13,3 +12,19 @@ def solution(n, m):
             answer.append(i)
             break
     return answer
+
+# ref1
+def gcdlcm(a, b):
+    c, d = max(a, b), min(a, b)
+    t = 1
+    while t > 0:
+        t = c%d
+        c, d = d, t
+    answer = [c, int (a*b/c)]
+    return answer
+
+# ref2
+def solution(n, m):
+    gcd = lambda a,b : b if not a%b else gcd(b, a%b)
+    lcm = lambda a,b : a*b//gcd(a,b)
+    return [gcd(n, m), lcm(n, m)]
