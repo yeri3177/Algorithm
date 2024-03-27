250aee1bcf9cf98a62ed7be7bacccca90b43650e
YERI <jyr3177@gmail.com>
2024-03-27 13:27:00 +0900

    [Bronze IV] Title: 주사위 세개, Time: 208 ms, Memory: 17732 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        System.out.println(solution(a,b,c));
+    }
+
+    public static int solution(int a, int b, int c){
+        int answer = 0;
+
+        if(a == b && b == c){
+            answer = 10000 + a*1000;
+        }else if(a == b || b == c || a == c){
+            answer = 1000 + (a==b?a:c)*100;
+        }else{
+            answer = Math.max(a,Math.max(b,c))*100;
+        }
+        return answer;
+    }
+}
