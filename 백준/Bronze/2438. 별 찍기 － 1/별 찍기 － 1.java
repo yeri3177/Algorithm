7019ed6dadda65b7a50f185838ec8912c88daa72
YERI <jyr3177@gmail.com>
2024-03-27 11:36:08 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 372 ms, Memory: 18140 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int row=1; row<=n; row++){
+            for(int column=1; column<=row; column++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
