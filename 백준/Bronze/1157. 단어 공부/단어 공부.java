12866fde1e263d791d80eb97c4c28ea51ec3825f
YERI <jyr3177@gmail.com>
2024-03-28 21:02:17 +0900

    [Bronze I] Title: 단어 공부, Time: 280 ms, Memory: 21100 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -1,8 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -10,19 +8,25 @@ public class Main {
         String str = br.readLine().toUpperCase();
 
         int[] alphabet = new int[26];
+        int strLength = str.length();
 
-        for (int i = 0; i < str.length(); i++) {
-            alphabet[str.charAt(i)-'A'] += 1;
+        for (int i = 0; i < strLength; i++) {
+            alphabet[str.charAt(i)-'A']++;
         }
 
-        int max = Arrays.stream(alphabet).max().getAsInt();
+        int max = Integer.MIN_VALUE;
         int count = 0;
         int index = -1;
 
-        for (int i = 0; i < alphabet.length; i++) {
-            if(alphabet[i] == max) {
-                count++;
+        for (int i = 0; i < 26; i++) {
+            if(alphabet[i] == 0) continue;
+
+            if (max < alphabet[i]) {
+                max = alphabet[i];
                 index = i;
+                count = 1;
+            } else if (max == alphabet[i]) {
+                count++;
             }
         }
 
