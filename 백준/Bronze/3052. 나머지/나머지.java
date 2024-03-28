75e8457b58a4062b7361854f718d07e21579a6a9
YERI <jyr3177@gmail.com>
2024-03-28 11:04:26 +0900

    [Bronze II] Title: 나머지, Time: 128 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+/**
+ * 나머지
+ * https://www.acmicpc.net/problem/3052
+ */
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Set<Integer> remainders = new HashSet<>();
+
+        for (int i = 0; i < 10; i++) {
+            remainders.add(Integer.parseInt(br.readLine())%42);
+        }
+
+        System.out.println(remainders.size());
+    }
+}
