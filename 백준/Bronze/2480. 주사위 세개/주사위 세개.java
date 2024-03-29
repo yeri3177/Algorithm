554321e2418f47738936a92045524538bea644e8
YERI <jyr3177@gmail.com>
2024-03-30 00:02:04 +0900

    [Bronze IV] Title: 주사위 세개, Time: 132 ms, Memory: 14160 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.java
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -1,25 +1,23 @@
-import java.util.Scanner;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        br.close();
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c = sc.nextInt();
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
 
-        System.out.println(solution(a,b,c));
-    }
-
-    public static int solution(int a, int b, int c){
-        int answer = 0;
-
-        if(a == b && b == c){
-            return 10000 + a*1000;
-        }else if(a == b || b == c || a == c){
-            return 1000 + (a==b?a:c)*100;
+        if(A == B && B == C){
+            System.out.println(10000 + A*1000);
+        }else if(A == B || B == C || A == C){
+            System.out.println(1000 + (A == B ? A : C) * 100);
         }else{
-            return Math.max(a,Math.max(b,c))*100;
+            System.out.println(Math.max(A, Math.max(B, C))*100);
         }
     }
 }
