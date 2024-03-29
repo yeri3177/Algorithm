89a707fe326739107f46015313ae2b102cac79fd
YERI <jyr3177@gmail.com>
2024-03-30 00:29:57 +0900

    [Bronze II] Title: 문자열 반복, Time: 116 ms, Memory: 13972 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.java
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.java
@@ -1,37 +1,30 @@
-
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        int count = sc.nextInt();
-        sc.nextLine();
-
-        String[] arr = new String[count];
-
-        for (int i = 0; i < count; i++) {
-            arr[i] = sc.nextLine();
-        }
-
-        solution(arr);
-    }
-
-    public static void solution(String[] arr){
-
-        for(int i=0; i<arr.length; i++){
-            int cnt = Integer.valueOf(arr[i].substring(0, 1)); // 반복 횟수
-            String str = arr[i].substring(2); // 문자열
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int cnt = Integer.parseInt(st.nextToken());
+            String str = st.nextToken();
 
             for (int j = 0; j < str.length(); j++) {
+                int count = 0;
 
-                for (int k = 0; k < cnt; k++) {
-                    System.out.print(str.charAt(j));
+                while (count < cnt) {
+                    sb.append(str.charAt(j));
+                    count++;
                 }
-
             }
-            System.out.println();
+            sb.append("\n");
         }
+        System.out.println(sb);
     }
-}
+ }
