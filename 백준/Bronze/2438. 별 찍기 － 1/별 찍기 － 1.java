9a871716306da0ca82af8a38cb1c749fe97e3f16
YERI <jyr3177@gmail.com>
2024-03-30 00:11:59 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 132 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -1,16 +1,20 @@
-import java.util.Scanner;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        br.close();
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        StringBuilder sb = new StringBuilder();
 
         for(int row=1; row<=n; row++){
             for(int column=1; column<=row; column++){
-                System.out.print("*");
+                sb.append("*");
             }
-            System.out.println();
+            sb.append("\n");
         }
+        System.out.println(sb);
     }
 }
