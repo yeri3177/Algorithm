9052bce1e617af1ae828122dc026522b331f7b95
YERI <jyr3177@gmail.com>
2024-04-01 01:53:37 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 212 ms, Memory: 18068 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int row = Integer.parseInt(st.nextToken());
+        int col = Integer.parseInt(st.nextToken());
+        int[][] arr = new int[row][col];
+        StringBuilder answer = new StringBuilder();
+
+        for (int i = 0; i < row; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < col; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int i = 0; i < row; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < col; j++) {
+                answer.append(arr[i][j] + Integer.parseInt(st.nextToken()) + " ");
+            }
+            answer.append("\n");
+        }
+        System.out.println(answer);
+        br.close();
+    }
+}
