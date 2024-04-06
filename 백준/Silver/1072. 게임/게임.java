545ff80881154af15618f607b6728b0ef3205064
YERI <jyr3177@gmail.com>
2024-04-06 23:13:18 +0900

    [Silver III] Title: 게임, Time: 124 ms, Memory: 14120 KB -BaekjoonHub

--- 백준/Silver/1072. 게임/게임.java
+++ 백준/Silver/1072. 게임/게임.java
@@ -3,30 +3,34 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-/**
- * [문제] 게임
- * 메모리    시간
- *
- * {(Y + a) / (X + a)} * 100 = Z + 1 -> a = ?
- * a = (ZX+X−100Y)/99−Z (Z<99)
- */
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        long X = Integer.parseInt(st.nextToken()); // 게임 횟수, 1이상 10억이하
-        long Y = Integer.parseInt(st.nextToken()); // 이긴 게임, 0이상 X이하
-        long Z = Y * 100 / X ; // 승률, 0이상 100이하
+        long X = Integer.parseInt(st.nextToken());
+        long Y = Integer.parseInt(st.nextToken());
+        long Z = Y * 100 / X ;
 
         if(Z >= 99) {
             System.out.println(-1);
             return;
         }
-        double answer = (double)(Z*X + X - 100 * Y)/ (99-Z);
 
-        // 소수점이 있으면 올림 해줘야함
-        System.out.println((long)Math.ceil(answer));
+        long start = 0;
+        long end = 1000000000;
+
+        while (start < end) {
+            long mid = (start + end) / 2;
+            long ret = (Y + mid) * 100 / (X + mid);
+
+            if (ret > Z) {
+                end = mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+        System.out.println(end);
         br.close();
     }
 }
