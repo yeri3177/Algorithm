386d43cd7c8fc1741a49cfa7a448a4380e74b07f
YERI <jyr3177@gmail.com>
2024-04-08 00:17:57 +0900

    [Silver I] Title: 추월, Time: 136 ms, Memory: 14376 KB -BaekjoonHub

--- 백준/Silver/2002. 추월/추월.java
+++ 백준/Silver/2002. 추월/추월.java
@@ -1,36 +1,48 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Iterator;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.StringTokenizer;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // 차의 대수 N(1 ≤ N ≤ 1,000)
         int N = Integer.parseInt(br.readLine());
-        Map<String, Integer> map = new LinkedHashMap<>();
 
-        for (int i = 0; i < N; i++) {
-            map.put(br.readLine(), i);
-        }
-
+        // 추월하는차 카운트 하기
         int count = 0;
 
-        for (int i = 0; i < N; i++) {
-            String car = br.readLine();
-
-            Iterator<String> iter = map.keySet().iterator();
-            while(iter.hasNext()){
-                if(map.get(car) > map.get(iter.next())){
-                    count++;
-                    break;
-                }
-            }
-            map.remove(car);
+        // 1.터널에 차가 들어가는 순서대로 큐에 넣기
+        Queue<String> enter = new LinkedList<>();
+        for(int i=0; i<N; i++) {
+            enter.add(br.readLine());
         }
 
+        // 2.터널에 차가 나오는 순서대로 큐에 넣기
+        Queue<String> exit = new LinkedList<>();
+        for(int i=0; i<N; i++) {
+            exit.add(br.readLine());
+        }
+
+        // 나가는 차가 담긴 큐가 비어있지 않을때까지 반복
+        while(!exit.isEmpty()) {
+            // 나가는차 앞에서 하나 빼기
+            String car = exit.poll();
+
+            // 나가는 차와 들어가는차가 같지 않으면
+            if(!enter.peek().equals(car)) {
+                count++; // 카운트 해주기
+                enter.remove(car); // enter큐에 해당 차 지우기
+            }
+            // 나가는 차와 들어가는차가 같으면
+            else {
+                enter.poll(); // 앞에꺼 지우기
+            }
+        }
+
+        // 추월한 차의 개수 출력
         System.out.println(count);
         br.close();
     }
