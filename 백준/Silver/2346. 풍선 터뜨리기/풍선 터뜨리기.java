e5071c768ecc312cf6dc8019d933a938e1429cca
YERI <jyr3177@gmail.com>
2024-04-07 13:55:24 +0900

    [Silver III] Title: 풍선 터뜨리기, Time: 172 ms, Memory: 15192 KB -BaekjoonHub

--- 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.java
+++ 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.java
@@ -1,54 +1,56 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
-
-class Balloon {
-    int index; // 풍선 자리 위치
-    int value; // 풍선에 적힌 숫자
-
-    public Balloon(int index, int value) {
-        this.index = index;
-        this.value = value;
-    }
-}
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine()); // 풍선 개수
+
+        Deque<int[]> deque = new ArrayDeque<>();
         StringBuilder sb = new StringBuilder(); // 출력용 문자열
-        Deque<Balloon> deque = new ArrayDeque<>(); // 풍선담을용으로 덱 사용
-        int n = Integer.parseInt(br.readLine()); // 풍선 개수
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        for(int i = 1; i <= n; i++){ // 자리는 1부터 n까지
-            // 덱에 풍선 넣기
-            deque.offer(new Balloon(i, Integer.parseInt(st.nextToken())));
+        int[] arr = new int[N]; // 풍선 숫자 담을 배열
+        for(int i=0; i<N; i++) { // 배열에 숫자 담기
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        // 풍선에 나온숫자를 맨앞자리로 위치하여 빼내는 방법
-        while(deque.size() > 1){
-            Balloon b = deque.poll(); // 앞에꺼 빼기
-            int m = b.value; // 풍선에 적힌 숫자
-            sb.append(b.index).append(" "); // 위치값 넣기
 
-            if(m > 0){ // 양수이면 오른쪽으로 이동
-                for(int i = 0; i < m-1; i++){
-                    Balloon tmp = deque.poll(); // 앞에서 빼서
-                    deque.offer(tmp); // 뒤에 넣기
+        // 덱에 풍선의 위치, 숫자가 있는 정수 배열 넣기
+        for(int i = 0; i < N; i++){
+            deque.add(new int[] {(i+1), arr[i]});
+        }
+
+        // 덱에서 첫번째부터 마지막-1까지 반복
+        while(deque.size() > 1) {
+            int[] balloon = deque.poll(); // 앞에꺼 빼기
+            int num = balloon[1]; // 풍선에 적힌 숫자
+            sb.append(balloon[0]).append(" "); // 위치값 넣기
+
+            // 양수인 경우
+            if(num > 0) {
+                // 순서 뒤로 돌리기
+                for(int i = 1; i < num; i++) {
+                    deque.add(deque.poll());
                 }
             }
-            else if(m < 0){ // 음수이면 왼쪽 이동
-                for(int i = 0; i < Math.abs(m); i++){
-                    Balloon tmp = deque.pollLast(); // 뒤에서 빼서
-                    deque.offerFirst(tmp); // 앞에 넣기
+
+            // 음수인 경우
+            else {
+                for(int i = 1; i <= -num; i++) {
+                    deque.addFirst(deque.pollLast());
                 }
             }
         }
 
-        Balloon m = deque.poll(); // 마지막값
-        sb.append(m.index); // 마지막값 위치 넣기
+        // 마지막 풍선 넣기
+        int[] balloon = deque.poll();
+        sb.append(balloon[0]);
 
-        System.out.println(sb);
+        System.out.println(sb.toString());
     }
 }
