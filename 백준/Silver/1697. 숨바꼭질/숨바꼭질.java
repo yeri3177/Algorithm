45588b3f77b31aa79552fd5715339bae037ffb55
YERI <jyr3177@gmail.com>
2024-04-09 16:51:32 +0900

    [Silver I] Title: 숨바꼭질, Time: 168 ms, Memory: 18860 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -1,53 +1,63 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int N;
-    static int K;
-    static int[] check = new int[100001];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st  = new StringTokenizer(br.readLine());
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        K = sc.nextInt();
+        // 수빈이가 있는 위치, N(0 ≤ N ≤ 100,000)
+        int N = Integer.parseInt(st.nextToken());
+        // 동생이 있는 위치, K(0 ≤ K ≤ 100,000)
+        int K = Integer.parseInt(st.nextToken());
 
-        if (N == K) {
-            System.out.println(0);
-        } else {
-            bfs(N);
-        }
+        // bfs 실행
+        System.out.println(bfs(N, K));
+        br.close();
     }
+    static int bfs(int N, int K) {
+        int time = 0;
 
-    static void bfs(int num) {
-        Queue<Integer> q = new LinkedList<>();
-        q.add(num);
-        check[num] = 1;
+        // 처음부터 위치 같으면 0 리턴
+        if(N == K) return time;
 
-        while (!q.isEmpty()) {
-            int temp = q.poll();
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(N);
+
+        // 인덱스 : 위치, 0~10만
+        // 값 : 찾는 시간
+        int[] visited = new int[100001];
+        visited[N] = 1;
+
+        loop:
+        while (!queue.isEmpty()) {
+            int now = queue.poll();
 
             for (int i = 0; i < 3; i++) {
                 int next;
 
                 if (i == 0) {
-                    next = temp + 1;
+                    next = now + 1;
                 } else if (i == 1) {
-                    next = temp - 1;
+                    next = now - 1;
                 } else {
-                    next = temp * 2;
+                    next = now * 2;
                 }
 
                 if (next == K) {
-                    System.out.println(check[temp]);
-                    return;
+                    time = visited[now];
+                    break loop;
                 }
-
-                if (next >= 0 && next < check.length && check[next] == 0) {
-                    q.add(next);
-                    check[next] = check[temp] + 1;
+                if (next >= 0 && next <= 100000 && visited[next] == 0) {
+                    queue.offer(next);
+                    visited[next] = visited[now] + 1;
                 }
             }
         }
+        return time;
     }
 }
