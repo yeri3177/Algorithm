0ce0a2894265a6ba9a6e6c41581089afe6581762
YERI <jyr3177@gmail.com>
2024-04-10 18:43:26 +0900

    [Gold IV] Title: 오큰수, Time: 984 ms, Memory: 145056 KB -BaekjoonHub

---
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < N; i++) {
+            while(!stack.isEmpty() && arr[stack.peek()] < arr[i]){
+                arr[stack.pop()] = arr[i];
+            }
+            stack.add(i);
+        }
+
+        while(!stack.isEmpty()){
+            arr[stack.pop()] = -1;
+        }
+
+        for (int i = 0; i < N; i++) {
+            sb.append(arr[i]).append(" ");
+        }
+
+        System.out.println(sb);
+        br.close();
+    }
+}
