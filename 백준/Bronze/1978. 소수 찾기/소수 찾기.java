2990d0e684b5bf3f4c01f9454fc15e6bd2914f33
YERI <jyr3177@gmail.com>
2024-04-13 02:09:10 +0900

    [Bronze II] Title: 소수 찾기, Time: 124 ms, Memory: 14096 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.java
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.java
@@ -6,23 +6,36 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
+        br.readLine();	// 주어진 수는 사용하지 않는다.
+
+        // 입력값 범위는 1000이하의 자연수
+        boolean[] primeArray = getPrimeArray(1000);
+
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int answer = 0;
-
-        for (int i = 0; i < N; i++) {
+        int cnt = 0;
+        while( st.hasMoreTokens() ) {
             int num = Integer.parseInt(st.nextToken());
-
-            for (int j = 2; j <= num; j++) {
-                if (j == num) {
-                    answer++;
-                }
-                if (num % j == 0) {
-                    break;
-                }
+            if( !primeArray[num] ) {
+                cnt++;
             }
         }
-        System.out.println(answer);
-        br.close();
+        System.out.println(cnt);
+    }
+
+    // 소수이면 false, 아니면 true
+    public static boolean[] getPrimeArray(int max) {
+        boolean[] primeArray = new boolean[max + 1];
+        primeArray[0] = true;
+        primeArray[1] = true;
+
+        for (int i = 2; i <= Math.sqrt(max); i++) {
+            if (primeArray[i]) continue;
+
+            for (int j = i * i; j < primeArray.length; j += i) {
+                primeArray[j] = true;
+            }
+        }
+
+        return primeArray;
     }
 }
