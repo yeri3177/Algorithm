53294f5145a06f3eecdfd041891b16cd5e3ede1d
YERI <jyr3177@gmail.com>
2024-04-16 00:04:42 +0900

    [Silver III] Title: 이친수, Time: 124 ms, Memory: 14212 KB -BaekjoonHub

---
+++ 백준/Silver/2193. 이친수/이친수.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        // 1 ≤ N ≤ 90
+        int N = Integer.parseInt(br.readLine());
+        long[] dp = new long[N + 1];
+
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for (int i = 2; i <= N; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]);
+        }
+
+        System.out.println(dp[N]);
+        br.close();
+    }
+}
